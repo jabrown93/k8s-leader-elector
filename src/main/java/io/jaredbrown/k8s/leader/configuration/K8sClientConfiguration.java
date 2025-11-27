1d18711ee4d92b024b6e1f021c2e6f6b617202a6
Jared Brown <github@jaredbrown.io>
2025-11-27 13:46:37 -0800

    fix bean

--- src/main/java/io/jaredbrown/k8s/leader/configuration/K8sClientConfiguration.java
+++ src/main/java/io/jaredbrown/k8s/leader/configuration/K8sClientConfiguration.java
@@ -2,11 +2,14 @@ package io.jaredbrown.k8s.leader.configuration;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class K8sClientConfiguration {
     @Bean
+    @PreDestroy
     public KubernetesClient kubernetesClient() {
         final KubernetesClientBuilder builder = new KubernetesClientBuilder();
         return builder.build();
